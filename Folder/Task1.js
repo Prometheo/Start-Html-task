bf41da92106554f9d2d8a696b0f99ffcd3b15d75
Popoola Boluwatife <olaoyejnr@gmail.com>
2020-03-24 11:51:03 +0000

    First javascript task.

---
+++ Folder/Task1.js
@@ -0,0 +1,14 @@
+//javascript code 
+
+Intern_name = "Popoola Boluwatife";
+My_courses = ["Python","Html","Css","Javascript"]
+Console.log(Intern_name)
+Console.log(My_courses)
+
+if (My_courses.length%2 == 0) {
+  for (let x=1; x<=200; x++) {
+     if (x%2 == 0) {
+       console.log(x)
+     }
+  }
+}
